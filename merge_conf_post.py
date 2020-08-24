434445f187ec4a15274d214ff7c7f7b617ecb250
Cristiano Nascimento <cristianonascimento98@gmail.com>
2020-08-24 10:55:02 -0300

    Merge feito mas n testado

--- merge_conf_post.py
+++ merge_conf_post.py
@@ -1,48 +1,57 @@
 import subprocess
 import sys
 
-def merge_files(filepost, fileconf):
+def merge_files(filepostname, fileconfname):
     
-    resultfile = 'result_post.txt'
+    resultfile = ''
 
-    file = open(filename, 'r')
+    filepost = open(filepostname, 'r')
 
-    lines = file.readlines()
+    fileconf = open(fileconfname, 'r')
 
-    newlines = []
+    linesPost = filepost.readlines()
 
-    print(len(lines))
+    linesConf = fileconf.readlines()
 
-    for line in lines:
+    newLinesPost = []
+
+    resultList = []
+
+    print(len(linesPost))
+    print(len(linesConf))
+
+    for line in linesPost:
         partes = line.split(' ] [ ')    
         aux = partes[0]
         teste = aux.split(' [ ')
         partes.remove(partes[0])
         partes.insert(0, teste[1])
-        partes.insert(0, teste[0])
+        #partes.insert(0, teste[0])
+        resultfile = 'result_' + teste[0] + '.txt'
         aux = partes[-1]
         partes.pop(-1)
         xua = aux.split(' ]')
         partes.append(xua[0])
-        newlines = partes
+        newLinesPost = partes
+
+    ind = 0
+    for line in linesConf:
+        partes = line.split(' ')
+        partes.pop(-1)
+        linhaPost = newLinesPost[ind]
+        aux = linhaPost.split(' ')
+        partes.append(aux[1])
+        linha = ' '.join(partes)
+        resultList.append(linha)
+        ind+=1
 
     with open(resultfile, 'w+') as filehandle:
-        filehandle.writelines("%s\n" % newline for newline in newlines)
-
-def roda_int2sym(filename, saida=''):
-    if saida != '':
-        comando = 'int2sym.pl -f 1 data/grammar/lang/words.txt ' + filename + ' > ' + saida
-    else:
-        comando = 'int2sym.pl -f 1 data/grammar/lang/words.txt ' + filename
-    output = subprocess.check_output(comando, shell=True, universal_newlines=True)
+        filehandle.writelines("%s\n" % newline for newline in resultList)
    
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Falta-lhe argumentos meu caro, voce pode colocar apenas o arquivo de entrada, ou o arquivo de entrada e saida, para o caso de nao querer visualizar no terminal.")
-    else:       
-        conserta_arquivo(sys.argv[1])
-        if len(sys.argv) == 2:
-            roda_int2sym(sys.argv[1])        
-        if len(sys.argv) == 3:
-            print("O arquivo de saida sera: %s"%(sys.argv[2]))
-            roda_int2sym(sys.argv[1], sys.argv[2])
+    if len(sys.argv) < 3:
+        print("Falta-lhe argumentos meu caro, eh necessario o arquivo post e o ctm-conf respectivamente.")
+    elif len(sys.argv) == 3:   
+        merge_files(sys.argv[1], sys.argv[2])
+    else:
+        print("Argumentos demais meu caro, manera ai")
